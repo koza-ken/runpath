dd2c54e40c20aef6a4a755c18fffb620000f8b73
kozaken <gkozaka4@gmail.com>
2025-06-24 19:21:19 +0900

    add:音声のloadとplay

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+const audioPlaying = document.getElementById("audio-play")
+const audioButton = document.getElementById("audio-btn")
+audioButton.addEventListener('click', () => {
+  // ボタンを連打すると、回数分再生するようにloadを追加
+  audioPlaying.load()
+  audioPlaying.play()
+})
